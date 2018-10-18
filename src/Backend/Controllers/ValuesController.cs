0ff056d1177b5488cc8646da78c873711b12bb1a
Pierre Plagnes <FX08258@adgroup.michelin.com>
2018-10-18 13:14:49 +0200

    vboxredirect.png n doc

--- src/Backend/Controllers/ValuesController.cs
+++ src/Backend/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Backend.Controllers
+namespace BackEnd.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
